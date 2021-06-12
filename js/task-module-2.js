28ec6c02fffcced6ab7fc5c34882946e110f2832
Bohdan Sulimovsky <84584712+NilsRondhuis@users.noreply.github.com>
2021-06-12 20:27:01 +0300

    1

---
+++ js/task-module-2.js
@@ -0,0 +1,126 @@
+// Задача 8
+
+// const fruits = ["apple", "peach", "pear", "banana"];
+
+// const lastElementIndex = fruits.length - 1;
+// const lastElement = fruits[lastElementIndex];
+
+// console.log(lastElement);
+
+
+// Задача 9
+// const getExtremeElements = (array) => {
+//     let a = [];
+//     const firstEl = array.shift();
+//     const lastEl = array.pop();
+//     a.push(firstEl);
+//     a.push(lastEl);
+
+//     return a;
+// }
+
+// console.log(getExtremeElements([1, 2, 3, 4, 5]));
+// console.log(getExtremeElements(["Earth", "Mars", "Venus"]));
+// console.log(getExtremeElements(["apple", "peach", "pear", "banana"]));
+
+
+// Задача 11
+/**
+ * Сервису гравировки украшений нужна функция, которая бы автоматически считала цену гравировки, в зависимости от количества слов и цены за слово.
+
+Объявлена функция calculateEngravingPrice(message, pricePerWord). Эта функция принимает строку, состоящую из слов разделённых только пробелами (параметр message) и цену гравировки одного слова (параметр pricePerWord).
+
+Напиши тело функции, чтобы она возвращала общую стоимость гравировки всех слов в строке. 
+  **/
+// 1) нужно строку превратить в массив
+// 2) Перебрать массив через счетчик
+// 3) Кол-во слов умножить на кол-во букв
+// 4) Написать вар куда +=
+
+// const calculateEngravingPrice = (message, pricePerWord) => {
+//     let newArray = message.split(' ');
+//     let total = 0;
+    
+//     for (let i = 0; i <= newArray.length; i += 1) {
+//         total = i * pricePerWord;
+//     }
+
+//     return total;
+// }
+
+// console.log(calculateEngravingPrice("JavaScript is in my blood", 10));
+// console.log(calculateEngravingPrice("JavaScript is in my blood", 20));
+// console.log(calculateEngravingPrice("Web-development is creative work", 40));
+// console.log(calculateEngravingPrice("Web-development is creative work", 20));
+
+
+// Задача 16
+
+/**
+ * Напиши функцию makeArray(firstArray, secondArray, maxLength) для создания нового массива со всеми элементами двух исходных firstArray и secondArray. Параметр maxLength содержит максимально допустимую длину нового массива.
+
+Если количество элементов нового массива больше maxLength, функция должна вернуть копию массива длиной maxLength элементов. В противном случае функция должна вернуть новый массив целиком.
+ */
+
+// 1) Нужно склеить 2 массива
+// 3) Создать пустой var кудаа кидать копию
+// 2) Передать условия
+
+// const makeArray = (firstArray, secondArray, maxLength) => {
+//     const newArray = firstArray.concat(secondArray);
+
+//     let copyArray;
+
+//     if (newArray.length >= maxLength) {
+//         copyArray = newArray.slice(0, maxLength);
+//     } else {
+//         copyArray = newArray.slice();
+//     }
+    
+//     return copyArray;
+// }
+
+// console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3));
+// console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4));
+// console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3));
+// console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2));
+// console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4));
+// console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0));
+
+
+// Задача 21
+// Напиши функцию findLongestWord(string) которая принимает произвольную строку состоящую только из слов разделённых пробелом (параметр string) и возвращает самое длинное слово в этой строке.
+
+// 1) Нужно узнать самое длинное слово
+// 2) Сделать из строки массив и перебрать повесить на каждое слово длину
+
+// const findLongestWord = (string) => {
+//     const newArray = string.split(' ');
+//     let longWord = 0;
+//     let finalWord = '';
+
+//     for (let i = 0; i < newArray.length - 1; i += 1) {
+//         if (longWord < newArray[i].length) {
+//             // console.log('length ARR: ', newArray[i].length);
+//             longWord = newArray[i].length;
+//             // console.log('LONG WORD: ', longWord);
+//             finalWord = newArray[i];
+//             // console.log('FINAL: ', finalWord);
+//         }
+//     }
+
+//     return finalWord;
+// }
+
+// console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
+// console.log(findLongestWord("Google do a roll"));
+// console.log(findLongestWord("May the force be with you"));
+
+
+// Задача 23
+
+
+
+
+
+
--- js/test.js
+++ js/test.js
@@ -1,29 +1,99 @@
-// 5) Проверить если корзину пуста - показать фразу "Пустая корзина" иначе показать фразу "В корзине {х} товаров"
-// где х - это количество товаров в корзине
-
-// 1) Написать ф-цию корзина и передать в нее произвольное параметр
-// 2) Проверить на наличие товара в корзине посчитать с помощью свойства
-// строки
-// 3) Если корзина не пустая будет записать кол-во товара, перебрать с помощью цикла
-// 4) Создать переменную внутри ф-ции куда нужно записать кол-во
-
-
-const basket = (product) => {
-    
-    if (product.length > 0) {
-       return `В корзине ${product.length} товаров`;
-    } 
-
-    return `Пустая корзина`;
-}
-
-
-console.log(basket([]));
-console.log(basket(["Хлеб", "Молоко", "Конфеты"]));
-console.log(basket(['Dildo', 'Machete', 'Ball', 'Knife']));
-
-
-
-
 // 6) Написать програму которая спрашивает у пользователя цены, он вписывает их через prompt, с помощью массивов
 // пока не нажмет отмену. После этого посчитать суму чисел и используя alert вывести результат на экран
+
+
+
+
+// 5) Написать ф - ю capitalize которая буде принимать строку и будет возвращать новую где каждое слово будет с большой буквы
+// capitalize('the quick brown fox') // 'The Quick Brown Fox '
+
+// 1) Создать Ф с одним параметром
+// 2) Превратить строку в массив и перебрать 
+// 3) Сделать первую букву в слове большой
+// 4) Обрезать букву у слова
+// 5) Создать массив для новых слов
+
+// const capitalize = string => {
+//     const arrayFromString = string.split(' ');
+    
+//     const newArray = [];
+
+//     for (let word of arrayFromString) {
+        
+//         let firstLetter = word[0].toUpperCase();
+        
+//         let restString = word.slice(1);
+        
+//         let finalString = `${firstLetter}${restString}`;
+       
+//         newArray.push(finalString);
+//     }
+
+//     return newArray.join(' ');
+// }
+
+// console.log(capitalize("the quick brown fox"));
+
+
+// 6) Напишите функции для работы с коллекцией обучающих курсов courses:
+
+// addCourse(name) - добавляет курс в конец коллекции
+// removeCourse(name) - удаляет курс из коллекции
+// updateCourse(oldName, newName)- изменяет имя на новое
+
+// addCourse('Express'); // ["HTML", "CSS", "JavaScript", "React", "PostgreSQL", "Express"];
+// addCourse('CSS'); // У вас уже есть такой курс
+// removeCourse('React'); // ["HTML", "CSS", "JavaScript", "PostgreSQL", "Express"];
+// removeCourse('Vue'); // Курс с таким именем не найден;
+// updateCourse('Express', 'NestJS'); // ["HTML", "CSS", "JavaScript", "PostgreSQL", "NestJS"];
+
+// const courses = ["HTML", "CSS", "JavaScript", "React", "PostgreSQL"];
+
+// const addCourse = name => {
+
+//     if (courses.includes(name)) {
+//         return 'У вас уже есть такой курс';
+//     }
+
+//     courses.push(name);
+//     return courses;
+// }
+
+// console.log(addCourse('Express'));
+// console.log(addCourse('CSS'));
+
+// 1) Написать условие входит ли данный курс в колекцию
+// 2) Если есть он как то его удалить проверить на индекс оф
+
+// const removeCourse = name => {
+
+//     if (courses.includes(name)) {
+//         const deleteCourses = courses.indexOf(name);
+//         courses.splice(deleteCourses, 1);
+//     } else {
+//         return 'Курс с таким именем не найден';
+//     }
+
+//     return courses;    
+// }
+
+// console.log(removeCourse('React'));
+// console.log(removeCourse('Vue'));
+
+// 1) Написать Ф с двумя параметрами
+// 2) Узнать на каком индексе курс который надо заменить
+// 3) Использовать методы для замены 
+
+// const updateCourse = (newCourse, update) => {
+//     const updateEl = courses.indexOf(update);
+//     courses.splice(updateEl, 1, newCourse);
+//     return courses;
+// }
+
+// console.log(updateCourse('Express', 'PostgreSQL'));
+
+
+
+
+
+
