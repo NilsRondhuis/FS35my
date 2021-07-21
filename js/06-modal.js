aefb65df33a504ee47398adc3b7b538a5050f701
Bohdan <84584712+NilsRondhuis@users.noreply.github.com>
2021-07-21 08:53:45 +0300

    21

--- js/06-modal.js
+++ js/06-modal.js
@@ -18,24 +18,25 @@ ref.btnOpenRef.addEventListener('click', openModal);
 ref.btnCloseRef.addEventListener('click', closeModal);
 
 function openModal() {
+    window.addEventListener('keydown', onEscKeyPress);  // закрыть с еск
     ref.backdrop.classList.remove('is-hidden');
 };
 
 function closeModal() {
+    window.removeEventListener('keydown', onEscKeyPress);  // закрыть с еск
     ref.backdrop.classList.add('is-hidden');
 }
 
 // 2. Закрыть по клику в бекдроп: onBackDropClick ???
 
-// ref.backdrop.addEventListener('click', onBackDropClick);
+ref.backdrop.addEventListener('click', onBackDropClick);
 
-// function onBackDropClick() {
-//     ref.backdrop.classList.add('is-hidden');
-// };
+function onBackDropClick(event) {
+    event.target.classList.add('is-hidden');
+};
 
 // 3. Закрыть по ESC: onEscapeKeypress  Escape
-ref.btnCloseRef.addEventListener('keydown', onEscapeKeypress);
-
-function onEscapeKeypress(event) {
-    event.key === 'Escape' ? ref.backdrop.classList.add('is-hidden') : null;
+function onEscKeyPress(event) {
+    console.log(event);
+    event.code === 'Escape' ? ref.backdrop.classList.add('is-hidden') : null;
 };
