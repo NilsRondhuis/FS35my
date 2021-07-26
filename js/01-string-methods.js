69d007d746f5b0d003b704d11c2dbbc9f4c99a97
Bohdan Sulimovsky <84584712+NilsRondhuis@users.noreply.github.com>
2021-07-26 09:28:23 +0300

    26

--- js/01-string-methods.js
+++ js/01-string-methods.js
@@ -15,11 +15,21 @@ replace()
 */
 
 const a = 'The quick brown fox jumps over the lazy dog. If the dog reacted, was it really lazy?';
+const a2 = 'Hello R2D3';
+const goldNumber = '098-666-77-88';
 
 const res = a.replace('dog', 'cat');
 const res2 = a.replace(/dog/gi, 'cat'); // синтаксис регулярного /выражения/ с ключами global, ignoreCase
 
-console.log('res---', res);
-console.log('res2---', res2);
-console.log(a);
+// console.log('res---', res);
+// console.log('res2---', res2);
+// console.log(a);
 
+// const res3 = a2.replace('2', '5');
+
+// console.log('res3 ---', res3);
+// console.log(a2);
+
+const res4 = goldNumber.replace(/[6]/g, '9');
+
+console.log(res4);
