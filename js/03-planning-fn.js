2831bd2a2dae400d136d5a2c119b2b629fd26ddb
Bohdan <84584712+NilsRondhuis@users.noreply.github.com>
2021-08-03 20:09:12 +0300

    3

--- js/03-planning-fn.js
+++ js/03-planning-fn.js
@@ -11,3 +11,56 @@ setTimeout(функция или код, задержка, параметр, ...
 setInterval(функция или код, задержка, параметр, ...параметр);
 */
 
+
+function showMessage(text, name) {
+    console.log(`${text}, ${name}!`);
+};
+
+// setTimeout(showMessage, 2000, 'Hello', 'word');
+// setInterval(showMessage, 500, 'Hello', 'Sergio');
+
+// сет интервал на рекурсивном сеттаймаут
+// function showNumber(num) {
+// 	console.log(num);
+// 	if (5 > num) {
+// 		setTimeout(showNumber, 1000, ++num);
+// 	}
+// };
+
+// setTimeout(showNumber, 1000, 1);
+
+
+/*
+Остановит работу сеттаймаут обьявленого ренее
+
+clearTimeout(var);
+*/
+
+// function showNumber(num) {
+// 	console.log(num);
+// 	let timeId = setTimeout(showNumber, 1000, ++num);
+// 	if (num === 6) {
+// 		clearTimeout(timeId);
+// 	}
+// };
+
+// setTimeout(showNumber, 1000, 1);
+
+
+/*
+Так же только интервал
+
+clearTimeout(var);
+*/
+
+// let result = 0;
+
+// function calcNum(num) {
+// 	result += num;
+// 	console.log(result);
+// 	if (result === 10) {
+// 		clearInterval(timeId);
+// 	}
+// };
+
+// let timeId = setInterval(calcNum, 1000, 1);
