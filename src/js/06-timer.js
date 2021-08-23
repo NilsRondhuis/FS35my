14d8f366ba5b5a7ac9b50a16c8083f9f87cdc52c
NilsRondhuis <84584712+NilsRondhuis@users.noreply.github.com>
2021-08-23 20:39:39 +0300

    23

--- src/js/06-timer.js
+++ src/js/06-timer.js
@@ -7,10 +7,10 @@ const refs = {
 };
 
 refs.startBtn.addEventListener('click', () => {
-  timer.start();
+  timer.start.bind(timer);
 });
 refs.stopBtn.addEventListener('click', () => {
-  timer.stop();
+  timer.stop.bind(timer);
 })
 
 class Timer {
@@ -30,7 +30,7 @@ class Timer {
     this.intervalId = setInterval(() => {
       const currentTime = Date.now();
       const deltaTime = currentTime - startTime;
-      const time = getTimeComponents(deltaTime);
+      const time = this.getTimeComponents(deltaTime);
 
       this.onTick(time)
     }, 1000)
@@ -39,36 +39,34 @@ class Timer {
   stop() {
     clearInterval(this.intervalId);
     this.isActive = false;
+    const time = this.getTimeComponents(0);
+    this.onTick(time);
   }
-  
-}
 
-const timer = new Timer({ onTick: updateClockface });
-
-
-
-
-/*
+  /*
    * - Принимает время в миллисекундах
    * - Высчитывает сколько в них вмещается часов/минут/секунд
    * - Возвращает обьект со свойствами hours, mins, secs
    * - Адская копипаста со стека 💩
-*/
-function getTimeComponents(time) {
-  const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-  const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-  const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+  */
+getTimeComponents(time) {
+  const hours = this.pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+  const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+  const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
 
   return { hours, mins, secs };
 }
 
-
-/*
+  /*
    * Принимает число, приводит к строке и добавляет в начало 0 если число меньше 2-х знаков
-*/
-function pad(value) {
+  */
+pad(value) {
   return String(value).padStart(2, '0');
 }
+  
+}
+
+const timer = new Timer({ onTick: updateClockface });
 
 /*
  * - Принимает время в миллисекундах
@@ -79,27 +77,3 @@ function updateClockface({ hours, mins, secs }) {
   refs.clockface.textContent = `${hours}:${mins}:${secs}`;
 }
 
-
-// const timer = {
-//   intervalId: null,
-//   isActive: false,
-//   start() {
-//     if (this.isActive) {
-//       return;
-//     } 
-//     const startTime = Date.now();
-//     this.isActive = true;
-
-//     this.intervalId = setInterval(() => {
-//       const currentTime = Date.now();
-//       const deltaTime = currentTime - startTime;
-//       const time = getTimeComponents(deltaTime);
-
-//       updateClockface(time);
-//     }, 1000)
-//   },
-//   stop() {
-//     clearInterval(this.intervalId);
-//     this.isActive = false;
-//   }
-// };
