ce260cbd8688be1714607a70617840b39ca594b1
NilsRondhuis <nilsrondhuis@ukr.net>
2021-09-25 11:31:45 +0300

    1

---
+++ src/js/refs.js
@@ -0,0 +1,6 @@
+export default {
+    form: document.querySelector('#form'),
+    btn: document.querySelector('.form-btn'),
+    list: document.querySelector('#root-template'),
+    input: document.querySelector('.form-btn'),
+}
