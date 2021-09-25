f426eb270591d9ab66859bb9ce2aa85f376a836d
NilsRondhuis <nilsrondhuis@ukr.net>
2021-09-25 16:37:27 +0300

    1

--- src/js/03-async.js
+++ src/js/03-async.js
@@ -3,47 +3,77 @@
 // Паралельные операции с Promise.all()
 // try...catch
 
-function getFruit(name) {
+async function getFruit(name) {
   const fruits = {
     strawberry: '🍓',
     kiwi: '🥝 ',
     apple: '🍎',
   };
 
-  return new Promise((resolve, reject) =>
-    setTimeout(() => resolve(fruits[name]), 500),
-  );
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(fruits[name]);
+    }, 500);
+  });
 }
 
-async function aMakeSmoothie() {
-  try {
-    console.time('aMakeSmoothie');
-    const apple = getFruit('apple');
-    const kiwi = getFruit('kiwi');
-    const berry = getFruit('strawberry');
+// Паралельные операции с Promise.all()
+async function asyncMakeSmoothie() {
+  console.time('asyncMakeSmoothie');
+  const apple = getFruit('apple');
+  const kiwi = getFruit('kiwi');
+  const strawberry = getFruit('strawberry');
 
-    const fruits = await Promise.all([apple, kiwi, berry]);
-    console.log(fruits);
-    console.timeEnd('aMakeSmoothie');
-
-    return fruits;
-  } catch (error) {
-    console.log('Ошибка');
-  }
+  const fruits = await Promise.all([apple, kiwi, strawberry]);
+  console.log(fruits);
+  console.timeEnd('asyncMakeSmoothie');
 }
+asyncMakeSmoothie()
 
-aMakeSmoothie();
+// Последовательные операции
+// async function asyncMakeSmoothie() {
+//   console.time('asyncMakeSmoothie');
+//   const apple = await getFruit('apple');
+//   console.log(apple);
 
-// async function fn () {}
+//   const kiwi = await getFruit('kiwi');
+//   console.log(kiwi);
+//   console.timeEnd('asyncMakeSmoothie');
+// }
+// asyncMakeSmoothie();
 
-// const fn  = async function () {}
 
-// const arr = async () => {}
+// Последовательные операции
+// async function asyncMakeSmoothie() {
+//   const apple = await getFruit('apple');
+//   console.log(apple);
 
-// const x = {
-//   async getname () {}
+//   const kiwi = await getFruit('kiwi');
+//   console.log(kiwi);
+// }
+// asyncMakeSmoothie()
+
+// function makeSmoothie() {
+//   getFruit('apple').then(apple => {
+//     console.log(apple);
+
+//     getFruit('kiwi').then(kiwi => {
+//       console.log(kiwi);
+//     })
+//   });
 // }
 
-// class X {
-//   async getName () {}
+
+
+// const fn = async function () {};  expression
+// const fn = async () => {};  declaration
+// const x = {                  method obj
+//   async getName() {  
+
+//   }
+// }
+// class Name {        method class
+//   async getName() {
+
+//   }
 // }
