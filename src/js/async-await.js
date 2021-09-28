f3f2c020ae62e2573706a6d119cfd7aa174c0926
NilsRondhuis <nilsrondhuis@ukr.net>
2021-09-28 11:19:24 +0300

    finish

--- src/js/async-await.js
+++ src/js/async-await.js
@@ -1,7 +1,22 @@
 const BASE_URL = 'https://jsonplaceholder.typicode.com/users';
 
+// async function getData(url) {
+//     const response = await fetch(url);
+//     const data = await response.json(); // можно ставить аваит можно нет, но лучше ставить
+//     return data;
+// }
+// getData(BASE_URL).then(data => console.log(data));
+
 async function getData(url) {
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data);
-}
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+        return data;
+    } catch (error) {
+        console.log(error);
+        return error;
+    }
+};
+
+getData(BASE_URL)
